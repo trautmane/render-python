a2ae5195851641aa005b24e972df5548d77d6b93
forrest collman <forrest.collman@gmail.com>
2017-09-19 17:02:43 -0700

    changing super to be first in init

--- renderapi/tilespec.py
+++ renderapi/tilespec.py
@@ -366,10 +366,11 @@ class ImagePyramid(OrderedDict):
     '''
 
     def __init__(self, mipMapLevels=[],*args,**kwargs):
+        super(ImagePyramid,self).__init__(*args,**kwargs)
         self.mipMapLevels = mipMapLevels
         for mml in mipMapLevels:
             self.update(mml)
-        super(ImagePyramid,self).__init__(*args,**kwargs)
+        
 
     def __getitem__(self, key):
         assert type(key) == int
